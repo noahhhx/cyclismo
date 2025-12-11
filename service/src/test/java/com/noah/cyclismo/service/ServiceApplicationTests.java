88b0c351de7190248ad86f378092c1551b265448
noahhhx <mrnoahgiles@gmail.com>
2025-12-11 20:33:17 +0000

    init

---
+++ service/src/test/java/com/noah/cyclismo/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noah.cyclismo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
